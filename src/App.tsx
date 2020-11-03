b489ae8c5df33567391d9811e2b6e5da72025998
Martin PELCAT <martinpelcat@hotmail.com>
2020-11-03 23:01:31 +0100

    init 🚀

---
+++ src/App.tsx
@@ -0,0 +1,4 @@
+import { FC } from "react";
+import "./app.css";
+
+export const App: FC = () => <div className="bg-blue-500">Hello !</div>;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: {
+    enabled: process.env.NODE_ENV === "production" ? true : false,
+    mode: "all",
+    content: ["./src/**/*.html", "./src/**/*.tsx"],
+  },
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
